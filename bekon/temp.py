53b8b1adfb5480c7a4ab6cf035dbf6061d5aa4f2
cooklee <sbbogus@gmail.com>
2021-11-20 11:50:47 +0100

    refactoring

--- bekon/temp.py
+++ bekon/temp.py
@@ -1,11 +1,16 @@
-def mnozenia(a, b):
-    ret_str = "<table><tr><th></th>"
+def create_header(b):
+    ret_str = ''
     for el in range(1, b + 1):
         ret_str += f"<th>{el}</th>"
     ret_str += f"</tr>\n"
+    return ret_str
+
+
+def mnozenia(a, b):
+    ret_str = "<table><tr><th></th>"
+    ret_str += create_header(b)
     for i in range(1, a + 1):
         ret_str += f"<tr><th>{i}</th>"
-
         for j in range(1, b + 1):
             ret_str += f"<td>{i * j}</td>"
         ret_str += "<tr>\n"
